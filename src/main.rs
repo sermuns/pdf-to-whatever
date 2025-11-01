fba8781769e530c98664c9b3ec9302c6adf555a8
Samuel Åkesson <samak519@student.liu.se>
2025-11-01 20:34:13 +0100

    Actualy fucking working

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use wasm_bindgen::prelude::*;
+use gloo::file::Blob;
 use gloo::utils::document;
 use image::ImageFormat;
 use once_cell::sync::Lazy;
 use pdfium_render::prelude::*;
 use std::io::Cursor;
 use std::rc::Rc;
+use wasm_bindgen::prelude::*;
 
 use std::collections::HashMap;
 
 use gloo::console::log;
 use gloo::file::callbacks::FileReader;
 use humansize::format_size;
-use web_sys::{DragEvent, Event, HtmlInputElement};
+use web_sys::{DragEvent, Event, HtmlInputElement, Url, WorkerGlobalScope};
 use yew::html::TargetCast;
 use yew::{Callback, Component, Context, Html, html};
 
 pub struct RenderedImage {
-    name: String,
+    stem: String,
     original_human_size: String,
     png_data: Vec<u8>,
     jpeg_data: Vec<u8>,
@@ -55,18 +56,18 @@ impl Component for App {
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Loaded(file) => {
-                self.readers.remove(&file.name);
+                self.readers.remove(&file.stem);
                 self.files.push(file);
                 true
             }
             Msg::Files(files) => {
                 for file in gloo::file::FileList::from(files.expect("files")).iter() {
                     let link = ctx.link().clone();
-                    let name = file.name().clone();
                     let file_type = file.raw_mime_type();
                     if file_type != "application/pdf" {
                         continue;
                     }
+                    let stem = file.name().trim_end_matches(".pdf").to_string();
                     let original_human_size = format_size(file.size(), humansize::BINARY);
 
                     let pdfium = self.pdfium.clone();
@@ -94,7 +95,7 @@ impl Component for App {
                                 .unwrap();
 
                             link.send_message(Msg::Loaded(RenderedImage {
-                                name,
+                                stem,
                                 original_human_size,
                                 png_data: png_buf.into_inner(),
                                 jpeg_data: jpeg_buf.into_inner(),
@@ -114,68 +115,63 @@ impl Component for App {
         });
 
         html! {
-            <>
-                <main>
-                    <h1>{ "pdf-to-whatever" }</h1>
-                    <label>
-                        <div
-                            id="drop-container"
-                            ondrop={ctx.link().callback(|event: DragEvent| {
-                                event.prevent_default();
-                                Msg::Files(event.data_transfer().unwrap().files())
-                            })}
-                            ondragover={&noop_drag}
-                            ondragenter={&noop_drag}
-                        >
-                            <p>{"Drop your images here or click to select"}</p>
-                        </div>
-                        <input
-                            type="file"
-                            accept="application/pdf"
-                            multiple={true}
-                            onchange={ctx.link().callback(move |e: Event| {
-                                let input: HtmlInputElement = e.target_unchecked_into();
-                                Msg::Files(input.files())
-                            })}
-                        />
-                    </label>
-                    <div id="processed">
-                        <div>{ "2025-10-17 närvarolista_2025-10-15.pdf"}</div>
-                        <div>{"2.05 MiB"}</div>
-                        <a class="download">
-                            <img src="static/download-1-svgrepo-com.svg" width="10" height="15" />
-                            {"PNG"}
-                        </a>
-                        <a class="download">
-                            <img src="static/download-1-svgrepo-com.svg" width="10" height="15" />
-                            {"JPG"}
-                        </a>
-                        { for self.files.iter().map(Self::view_file) }
-                    </div>
-                </main>
-                <footer>
-                    {"Created by "}
-                    <a href="https://samake.se" target="_blank">
-                        {"Samuel \"sermuns\" Åkesson"}
-                    </a>
-                </footer>
-            </>
+        <main>
+            <h1>{ "pdf-to-whatever" }</h1>
+            <label>
+                <div
+                    id="drop-container"
+                    ondrop={ctx.link().callback(|event: DragEvent| {
+                        event.prevent_default();
+                        Msg::Files(event.data_transfer().unwrap().files())
+                    })}
+                    ondragover={&noop_drag}
+                    ondragenter={&noop_drag}
+                >
+                    <p>{"Drop your documents here or click to select"}</p>
+                </div>
+                <input
+                    type="file"
+                    accept="application/pdf"
+                    multiple={true}
+                    onchange={ctx.link().callback(move |e: Event| {
+                        let input: HtmlInputElement = e.target_unchecked_into();
+                        Msg::Files(input.files())
+                    })}
+                />
+            </label>
+            <div id="processed">
+                <div>{ "2025-10-17 närvarolista_2025-10-15.pdf"}</div>
+                <div>{"2.05 MiB"}</div>
+                <a class="download">
+                    <img src="download-1-svgrepo-com.svg" width="10" height="15" />
+                    {"PNG"}
+                </a>
+                <a class="download">
+                    <img src="download-1-svgrepo-com.svg" width="10" height="15" />
+                    {"JPG"}
+                </a>
+                { for self.files.iter().map(Self::view_file) }
+            </div>
+        </main>
         }
     }
 }
 
 impl App {
     fn view_file(file: &RenderedImage) -> Html {
+        let png_blob = Blob::new::<&[u8]>(&file.png_data);
+        let png_url = Url::create_object_url_with_blob(&png_blob.into())
+            .expect("failed creating url for png");
         html! {
             <>
-                <div>{ &file.name }</div>
+                <div>{ &file.stem }</div>
                 <div>{ &file.original_human_size }</div>
-                <a class="download">
-                    <img src="static/download-1-svgrepo-com.svg" width="10" height="15" />
+                <a class="download" href={png_url} target="_blank" download={file.stem.clone() + ".png"}>
+                    <img src="download-1-svgrepo-com.svg" width="10" height="15" />
                     {"PNG"}
                 </a>
                 <a class="download">
-                    <img src="static/download-1-svgrepo-com.svg" width="10" height="15" />
+                    <img src="download-1-svgrepo-com.svg" width="10" height="15" />
                     {"JPG"}
                 </a>
             </>
